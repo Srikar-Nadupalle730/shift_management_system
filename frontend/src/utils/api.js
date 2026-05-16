5a14a7b09d4d8ee0094ed852c7f85e4e028afa45
Srikar <nadupalle.srikar@gmail.com>
2026-05-16 19:23:41 +0530

    Did vercel customization

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   withCredentials: true,
   headers: {
     'Accept': 'application/json',
